2daeca8d5d63f86dc0d80bdb376b68ec91293223
mauriceweber96 <weber.maurice@icloud.com>
2022-05-28 23:45:01 +0200

    add: token_contract

--- stable_swap/tests/harness.rs
+++ stable_swap/tests/harness.rs
@@ -5,25 +5,37 @@ use fuels::test_helpers;
 
 // Load abi from json
 abigen!(MyContract, "out/debug/stable_swap-abi.json");
+abigen!(TestToken,"../token_contract/out/debug/token_contract-abi.json");
 
-async fn get_contract_instance() -> (MyContract, ContractId) {
+async fn get_contract_instance() -> (MyContract, ContractId, TestToken, ContractId) {
     // Launch a local network and deploy the contract
     let wallet = launch_provider_and_get_wallet().await;
 
-    let id = Contract::deploy("./out/debug/stable_swap.bin", &wallet, TxParameters::default())
+    let contract_id = Contract::deploy("./out/debug/stable_swap.bin", &wallet, TxParameters::default())
         .await
         .unwrap();
 
-    let instance = MyContract::new(id.to_string(), wallet);
+    let contract_instance = MyContract::new(contract_id.to_string(), wallet.clone());
 
-    (instance, id)
+    // Get the contract ID and a handle to it
+    let token_id =
+        Contract::deploy(
+            "../token_contract/out/debug/token_contract.bin",
+            &wallet,
+            TxParameters::default()
+        )
+        .await
+        .unwrap();
+    let token_instance = TestToken::new(token_id.to_string(), wallet.clone());
+
+    (contract_instance, contract_id, token_instance, token_id)
 }
 
 #[tokio::test]
 async fn can_deposit() {
-    let (_instance, _id) = get_contract_instance().await;
+    let (_contract_instance, _contract_id, _token_instance, _token_id) = get_contract_instance().await;
 
-    _instance
+    _contract_instance
         .deposit()
         .call_params(CallParameters::new(Some(11), None))
         .call()
@@ -35,9 +47,9 @@ async fn can_deposit() {
 
 #[tokio::test]
 async fn can_get_balance() {
-    let (_instance, _id) = get_contract_instance().await;
+    let (_contract_instance, _contract_id, _token_instance, _token_id) = get_contract_instance().await;
 
-    _instance
+    _contract_instance
         .deposit()
         .call_params(CallParameters::new(Some(11), None))
         .call()
@@ -47,7 +59,7 @@ async fn can_get_balance() {
     // Native asset id
     let native_asset_id = ContractId::new(*NATIVE_ASSET_ID);
 
-    let response = _instance
+    let response = _contract_instance
         .get_balance(native_asset_id)
         .call()
         .await
@@ -59,10 +71,10 @@ async fn can_get_balance() {
 
 #[tokio::test]
 async fn can_withdraw() {
-    let (_instance, _id) = get_contract_instance().await;
+    let (_contract_instance, _contract_id, _token_instance, _token_id) = get_contract_instance().await;
 
     // Depost some native assets
-    _instance
+    _contract_instance
         .deposit()
         .call_params(CallParameters::new(Some(11), None))
         .call()
@@ -73,14 +85,14 @@ async fn can_withdraw() {
     let native_asset_id = ContractId::new(*NATIVE_ASSET_ID);
 
     // Check contract balance
-    let response = _instance
+    let response = _contract_instance
         .get_balance(native_asset_id.clone())
         .call()
         .await
         .unwrap();
     assert_eq!(response.value, 11);
 
-    _instance
+    _contract_instance
         .withdraw(11, native_asset_id.clone())
         .append_variable_outputs(1)
         .call()
@@ -88,10 +100,16 @@ async fn can_withdraw() {
         .unwrap();
     
     // Check contract balance
-    let response = _instance
+    let response = _contract_instance
         .get_balance(native_asset_id)
         .call()
         .await
         .unwrap();
     assert_eq!(response.value, 0);
+}
+
+#[tokio::test]
+async fn can_add_liquidity() {
+    let (_contract_instance, _contract_id, _token_instance, _token_id) = get_contract_instance().await;
+
 }
